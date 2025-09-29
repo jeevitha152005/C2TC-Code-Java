a571456921a1cab306e067e25f2430aac9837797
jeevitha <jeevitha@LAPTOP-HJB03IPT>
2025-09-29 10:07:47 +0530

    This is core java programming

---
+++ corejava/src/day1/sample.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class sample {
+
+	public sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+             System.out.print("Welcome to java");
+	}
+
+}
+
